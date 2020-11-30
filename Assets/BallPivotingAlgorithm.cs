74f9766dc3ad0d38084ef14f134db9c1a32e0ee7
Devorenge <samuel.orwoll@gmail.com>
2020-11-30 11:23:38 +0100

    more work on the pivoter

--- Assets/BallPivotingAlgorithm.cs
+++ Assets/BallPivotingAlgorithm.cs
@@ -58,6 +58,14 @@ class Triangle {
 	public PointNormal First { get; set; }
 	public PointNormal Second { get; set; }
 	public PointNormal Third { get; set; }
+
+	public Triangle() {
+		First = Second = Third = null;
+	}
+
+	public Triangle(PointNormal p0, PointNormal p1, PointNormal p2, int index0, int index1, int index2, PointNormal ballCenter, float ballRadius) {
+
+	}
 }
 
 class Edge {
@@ -179,7 +187,82 @@ class Pivoter {
 	}
 
 	internal Triangle FindSeed() {
-		return null;
+		float neighborhoodSize = 1.3f;
+
+		Triangle seed = new Triangle();
+		bool found = false;
+		SortedDictionary<ulong, bool> tested = new SortedDictionary<ulong, bool>();
+		List<int> removeIndices = new List<int>();
+
+		foreach (KeyValuePair<int, bool> pair in notUsed) {
+			if (found) break;
+
+			int index0 = pair.Key;
+			if (pair.Value) continue;
+
+			List<int> indices = GetNeighbors(cloud[index0], ballRadius * neighborhoodSize);
+			if (indices.Count < 3)
+				continue;
+
+			for (int j = 0; j < indices.Count; j++) {
+				if (!found) {
+					int index1 = indices[j];
+
+					bool tmpVal1;
+					if (index1 == index0 || !notUsed.TryGetValue(index1, out tmpVal1) || tmpVal1)
+						continue;
+
+					for (int k = 0; k < indices.Count && !found; k++) {
+						int index2 = indices[k];
+
+						bool tmpVal2;
+						if (index1 == index2 || index2 == index0 || !notUsed.TryGetValue(index2, out tmpVal2) || tmpVal2)
+							continue;
+
+						List<int> trio = new List<int>();
+						trio.Add(index0);
+						trio.Add(index1);
+						trio.Add(index2);
+						trio.Sort();
+						ulong code = Convert.ToUInt64(trio[0]) + Convert.ToUInt64(1e6 * trio[1]) + Convert.ToUInt64(1e12 * trio[2]);
+
+						bool toContinue = false;
+
+						if (tested.TryGetValue(code, out _)) toContinue = true;
+						else tested[code] = true;
+
+						if (toContinue) continue;
+
+						Vector3 center;
+						Vector3Int sequence;
+
+						if(!found && GetBallCenter(index0, index1, index2, out center, out sequence)) {
+							PointNormal ballCenter = new PointNormal(center.x, center.y, center.z);
+							List<int> neighborhood = GetNeighbors(ballCenter, ballRadius);
+							if (!found && isEmpty(neighborhood, index0, index1, index2, center)) {
+
+								seed = new Triangle(cloud[sequence[0]], cloud[sequence[1]], cloud[sequence[2]], sequence[0], sequence[1], sequence[2], ballCenter, ballRadius);
+
+								removeIndices.Add(index0);
+								removeIndices.Add(index1);
+								removeIndices.Add(index2);
+
+								notUsed[index0] = true;
+								notUsed[index1] = true;
+								notUsed[index2] = true;
+
+								found = true;
+							}
+						}
+					}
+				}
+			}
+		}
+		foreach (var index in removeIndices) {
+			notUsed.Remove(index);
+		}
+
+		return seed;
 	}
 
 	internal PointNormal GetPoint(int index) {
@@ -194,8 +277,28 @@ class Pivoter {
 		notUsed.Remove(index);
 	}
 
-	Tuple<Vector3, float> GetCircumScribedCircle(Vector3 p0, Vector3 p1, Vector3 p2) {
-		return null;
+	Tuple<Vector3, float> GetCircumscribedCircle(Vector3 p0, Vector3 p1, Vector3 p2) {
+		Vector3 d10 = p1 - p0;
+		Vector3 d20 = p2 - p0;
+		Vector3 d01 = p0 - p1;
+		Vector3 d12 = p1 - p2;
+		Vector3 d21 = p2 - p1;
+		Vector3 d02 = p0 - p2;
+
+		float mag01 = d01.magnitude;
+		float mag12 = d12.magnitude;
+		float mag02 = d02.magnitude;
+
+		float mag01C12 = Vector3.Cross(d01, d12).magnitude;
+
+		float alpha = (mag12 * mag12 * d01.Dot(d02)) / (2 * mag01C12 * mag01C12);
+		float beta = (mag02 * mag02 * d10.Dot(d12)) / (2 * mag01C12 * mag01C12);
+		float gamma = (mag01 * mag01 * d20.Dot(d21)) / (2 * mag01C12 * mag01C12);
+
+		Vector3 circumscribedCircleCenter = alpha * p0 + beta * p1 + gamma * p2;
+		float circumscribedCircleRadius = (mag01 * mag12 * mag02) / (2 * mag01C12);
+
+		return new Tuple<Vector3, float>(circumscribedCircleCenter, circumscribedCircleRadius);
 	}
 
 	bool GetBallCenter(int index0, int index1, int index2, out Vector3 center, out Vector3Int sequence) {
@@ -223,7 +326,7 @@ class Pivoter {
 				normal = Vector3.Cross(v10, v20).normalized;
 			}
 
-			Tuple<Vector3, float> circle = GetCircumScribedCircle(p0, p1, p2);
+			Tuple<Vector3, float> circle = GetCircumscribedCircle(p0, p1, p2);
 			float squaredDistance = ballRadius * ballRadius - circle.Item2 * circle.Item2;
 			if(squaredDistance > 0) {
 				float distance = Mathf.Sqrt(Mathf.Abs(squaredDistance));
